64eb753f7d596f679e27539eeef5945a8a37244b
Amanda Dolci <amanda.dolci@gmail.com>
2024-06-02 22:21:35 -0300

    feat: create generic team page

---
+++ team.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Conheça nossa Equipe</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="sass/styles.css"> -->
+</head>
+
+<body>
+  <?php include 'includes/header.php'; ?>
+
+  <div class="container">
+    <h1 class="text-center custom-title">Conheça nossa Equipe</h1>
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-3 text-center">
+        <div class="card aos-init aos-animate" data-aos="fade-left" data-aos-delay="50">
+          <img src="assets/robin-buckley.webp" alt="Robin Buckley" class="card-img-top" loading="lazy">
+          <div class="card-body text-center">
+            <p><strong>Robin Buckley</strong></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-3 text-center">
+        <div class="card aos-init aos-animate" data-aos="fade-left" data-aos-delay="50">
+          <img src="assets/steve-harrington.webp" alt="Steve Harrington" class="card-img-top" loading="lazy">
+          <div class="card-body text-center">
+            <p><strong>Steve Harrington</strong></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-3 text-center">
+        <div class="card aos-init aos-animate" data-aos="fade-left" data-aos-delay="50">
+          <img src="assets/erica-sinclair.webp" alt="Erica Sinclair" class="card-img-top" loading="lazy">
+          <div class="card-body text-center">
+            <p><strong>Erica Sinclair</strong></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-3 text-center">
+        <div class="card aos-init aos-animate" data-aos="fade-left" data-aos-delay="50">
+          <img src="assets/eddie-munson.webp" alt="Eddie Munson" class="card-img-top" loading="lazy">
+          <div class="card-body text-center">
+            <p><strong>Eddie Munson</strong></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
